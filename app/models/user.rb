3de322e230059b85de8bd0ebf8d3ce173367416e
Arnaud Levy <alevy@lespoupees.paris>
2019-07-03 14:22:46 +0200

    users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :refs
+
   def to_s
     "#{email}"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index, :show]
   resources :refs
   get 'extension' => 'extension#hello'
   post 'extension' => 'extension#receive'
